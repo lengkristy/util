e8d68b2d747de141b04fea2327c9c9e9ad1072b3
代浩然 <daihaoran@qq.com>
2020-03-03 10:02:06 +0800

    增加递归创建文件夹

---
+++ src/main/java/com/moon/util/io/PathUtil.java
@@ -0,0 +1,19 @@
+/**
+ * projectName: util
+ * fileName: PathUtil.java
+ * packageName: com.moon.util.io
+ * date: 2020-03-02 15:23
+ * copyright(c) 2017-2020 同方赛威讯信息技术公司
+ */
+package com.moon.util.io;
+
+/**
+ * @version: V1.0
+ * @author: 代浩然
+ * @className: PathUtil
+ * @packageName: com.moon.util.io
+ * @description: 路径操作帮助类
+ * @data: 2020-03-02 15:23
+ **/
+public class PathUtil {
+}
